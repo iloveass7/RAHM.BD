3344b31ed7eb8dffb1b5b26ada14969575330f90
alimbinyeasin <alimbinyeasin@gmail.com>
2025-08-28 11:06:40 +0600

    dashboard

---
+++ RAHM.BD/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AdminController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Admin Dashboard";
+        return View();
+    }
+
+    public IActionResult Users()
+    {
+        ViewData["Title"] = "View Users";
+        return View();
+    }
+
+    public IActionResult Vaccine()
+    {
+        ViewData["Title"] = "Vaccine Management";
+        return View();
+    }
+
+    public IActionResult SendSms()
+    {
+        ViewData["Title"] = "Send SMS";
+        return View();
+    }
+
+    public IActionResult SendMail()
+    {
+        ViewData["Title"] = "Send Mail";
+        return View();
+    }
+
+    public IActionResult UploadContent()
+    {
+        ViewData["Title"] = "Upload Content";
+        return View();
+    }
+}
