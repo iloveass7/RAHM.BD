aa463b8f9aacd1b3afa954b45255fa7a3ee3ea7f
iloveass7 <borshonjsr@gmail.com>
2025-08-31 13:29:05 +0600

    admin users+healthcare center+ rog jibanu

--- RAHM.BD/obj/Debug/net9.0/RAHM.BD.AssemblyInfo.cs
+++ RAHM.BD/obj/Debug/net9.0/RAHM.BD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RAHMBD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+630411a36558f2d52ce315f680e12a1f7a1689f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eccc514f6c42cb9562ce8cff0ebc960c17146f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RAHMBD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RAHMBD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
